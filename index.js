09fb2b70c2b63fe7d49aea322a1f253021e3aad4
pasindu200018 <167278835+pasindu200018@users.noreply.github.com>
2024-05-25 06:38:10 +0000

    update

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const app = express();
 
+// middleware 
+app.use(bodyParser.json())
 
-const data =(req, res) => {
-    res.send('Hello World!');
-}
-app.get('/get', data )
+
+//post
+app.post('/post',(request,response)=>{
+
+    const {number1,number2} = request.body
+   
+
+    var total = number1 + number2
+    console.log(total)
+    
+    response.json({total})
+})
 
 app.listen(5000,()=>{
     console.log('server is running port 3000')
